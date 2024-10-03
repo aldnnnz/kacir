c060037c89a6a717260f2b2c63e4d7dda307dd5d
aldnnnz <sayabukanrobot999@gmail.com>
2024-10-03 13:59:13 +0700

    add color

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+    
 }
--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -27,8 +27,14 @@ class DashboardPanelProvider extends PanelProvider
             ->id('dashboard')
             ->path('/')
             ->login()
+            ->registration()
             ->colors([
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
                 'primary' => Color::Green,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
